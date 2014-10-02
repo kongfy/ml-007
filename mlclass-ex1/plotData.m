2f4ce6ac03fee567752716419ca5f0ad0aa45d67
Fanyu Kong <njukongfy@gmail.com>
2014-10-03 00:29:00 +0800

    all

--- mlclass-ex1/plotData.m
+++ mlclass-ex1/plotData.m
@@ -20,7 +20,6 @@ plot(x, y, 'rx', 'MarkerSize', 5);      % Plot the data
 ylabel('Profit in $10,000s');            % Set the y−axis label
 xlabel('Population of City in 10,000s'); % Set the x−axis label
 
-
 % ============================================================
 
 end
